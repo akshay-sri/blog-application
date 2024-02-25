e4d18e5116906ca84b61a0d027a5a3bd09731383
akshay-sri <sriakshay2611999@gmail.com>
2024-02-26 00:12:17 +0530

    change

--- src/main/java/com/blog/project/payloads/JwtAuthRequest.java
+++ src/main/java/com/blog/project/payloads/JwtAuthRequest.java
@@ -1,14 +1,11 @@
 package com.blog.project.payloads;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class JwtAuthRequest {
-    @Schema(example = "akshay@gmail.com")
     private String username;
-    @Schema(example = "akshay123")
     private String password;
 }
